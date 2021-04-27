5161247cc1b8d2027ef9110a184ca1dc5ced0385
Marc Lalonde <marc.lalonde@crim.ca>
2021-04-27 13:59:37 -0400

    Add validation stage to the training loop

--- source/training.cpp
+++ source/training.cpp
@@ -1,9 +1,5 @@
-//
-//  main.cpp
-//  transfer-learning
-//
-//  Created by Kushashwa Ravi Shrimali on 12/08/19.
-//
+#include <algorithm>
+#include <vector>
 #include<fstream>
 #include <torch/torch.h>
 #include "torchvision/models/resnet.h"
@@ -14,8 +10,35 @@
 #include "models/EfficientNet.h"
 #include "models/SGD_AGC.h"
 
+using DataSamples_t = std::pair<std::vector<std::string>, std::vector<int>>;
 
-constexpr auto OUTPUT_FNAME = "sgdagc_clip0.1_lr1.0.txt";
+constexpr auto OUTPUT_FNAME = "testsgdagc_clip0.1_lr1.0.txt";
+
+void splitData(DataSamples_t srcPairs, double splitProportion, DataSamples_t& trainingPairs, DataSamples_t& validationsPairs) {
+
+    std::vector<int> vIndices;
+    for (int i = 0; i < srcPairs.first.size(); i++)
+        vIndices.push_back(i);
+
+    std::vector<int>::iterator bound;
+    bound = std::partition(vIndices.begin(), vIndices.end(), [&](auto a) { 
+        double aa = (double)std::rand() / (RAND_MAX + 1u);
+        return  aa < splitProportion; });
+
+    for (std::vector<int>::iterator it = vIndices.begin(); it != bound; ++it) {
+        trainingPairs.first.push_back(srcPairs.first.at(*it));
+        trainingPairs.second.push_back(srcPairs.second.at(*it));
+    }
+
+    
+    for (std::vector<int>::iterator it = bound; it != vIndices.end(); ++it)
+    {
+        validationsPairs.first.push_back(srcPairs.first.at(*it));
+        validationsPairs.second.push_back(srcPairs.second.at(*it));
+    }
+
+}
+
 
 
 torch::Tensor read_data(std::string location) {
@@ -114,11 +137,11 @@ std::pair<std::vector<std::string>,std::vector<int>> load_data_from_folder(std::
         if((dir = opendir(base_name.c_str())) != NULL) {
             while((ent = readdir(dir)) != NULL) {
                 std::string filename = ent->d_name;
-                std::cout << filename << std::endl;
+//                std::cout << filename << std::endl;
                 if(filename.length() > 4 && filename.substr(filename.length() - 4) == "JPEG") {
                     // cout << base_name + ent->d_name << endl;
                     // cv::Mat temp = cv::imread(base_name + "/" + ent->d_name, 1);
-                    std::cout << "push ----"<< base_name + ent->d_name << "----"<<std::endl;
+//                    std::cout << "push ----"<< base_name + ent->d_name << "----"<<std::endl;
                     list_images.push_back(base_name + ent->d_name);
                     list_labels.push_back(label);
                 }
@@ -134,7 +157,7 @@ std::pair<std::vector<std::string>,std::vector<int>> load_data_from_folder(std::
 }
 
 template<typename Dataloader>
-void train(torch::nn::ModuleHolder<EfficientNetV1Impl>& net, Dataloader& data_loader, torch::optim::Optimizer& optimizer, size_t dataset_size) {
+void train(torch::nn::ModuleHolder<EfficientNetV1Impl>& net, Dataloader& data_loader_trn, Dataloader& data_loader_valid, torch::optim::Optimizer& optimizer, int size_trn, int size_valid) {
     /*
      This function trains the network on our data loader using optimizer.
      
@@ -153,14 +176,17 @@ void train(torch::nn::ModuleHolder<EfficientNetV1Impl>& net, Dataloader& data_lo
      */
     float best_accuracy = 0.0; 
     int batch_index = 0;
-    
+    std::cout << "Training set size: " << size_trn << std::endl;
+    std::cout << "Validation set size: " << size_valid << std::endl;
+    std::cout << "Saving stats in " << OUTPUT_FNAME << std::endl;
     std::ofstream MyFile(OUTPUT_FNAME, std::ios::out);
 
     for(int i=0; i<25; i++) {
         float mse = 0;
         float Acc = 0.0;
+        float valid_acc = 0.0;
         
-        for(auto& batch: *data_loader) {
+        for(auto& batch: *data_loader_trn) {
             auto data = batch.data;
             auto target = batch.target.squeeze();
             
@@ -191,9 +217,26 @@ void train(torch::nn::ModuleHolder<EfficientNetV1Impl>& net, Dataloader& data_lo
             batch_index += 1;
         }
 
+        for (auto& batch : *data_loader_valid) {
+            auto data = batch.data;
+            auto target = batch.target.squeeze();
+
+            // Should be of length: batch_size
+            data = data.to(torch::kF32).to(torch::kCUDA);
+            target = target.to(torch::kInt64).to(torch::kCUDA);
+
+            auto output = net.get()->forward(data);
+            output = output.view({ output.size(0), -1 });
+            auto acc = output.argmax(1).eq(target).sum();
+
+            valid_acc += acc.template item<float>();
+        }
+
+
         mse = mse/float(batch_index); // Take mean of loss
-        std::cout << "Epoch: " << i  << ", " << "Accuracy: " << Acc/dataset_size << ", " << "MSE: " << mse << std::endl;
-        MyFile << Acc / dataset_size << std::endl;
+        std::cout << "Epoch: " << i  << ", " << "MSE: " << mse << ", training accuracy: " << Acc/size_trn<< ", validation accuracy: " << valid_acc / size_valid << std::endl;
+        MyFile << mse << " "<< Acc / size_trn << " " << valid_acc/size_valid << std::endl;
+
         /*test(net, lin, data_loader, dataset_size);
 
         if(Acc/dataset_size > best_accuracy) {
@@ -252,21 +295,29 @@ void test(torch::jit::script::Module network, torch::nn::Linear lin, Dataloader&
 int main(int argc, const char * argv[]) {
 
 
-    // Set folder names for cat and dog images
     std::string dataset_folder_cls1 = "M:\\data22-brs\\AARISH\\01\\nobackup\\imagenet\\training_256x256_rgb\\train\\n01440764";
     std::string dataset_folder_cls2 = "M:\\data22-brs\\AARISH\\01\\nobackup\\imagenet\\training_256x256_rgb\\train\\n01443537";
     std::string dataset_folder_cls3 = "M:\\data22-brs\\AARISH\\01\\nobackup\\imagenet\\training_256x256_rgb\\train\\n01484850";
+    std::string dataset_folder_cls1_val = "M:\\data22-brs\\AARISH\\01\\nobackup\\imagenet\\training_256x256_rgb\\val\\n01440764";
+    std::string dataset_folder_cls2_val = "M:\\data22-brs\\AARISH\\01\\nobackup\\imagenet\\training_256x256_rgb\\val\\n01443537";
+    std::string dataset_folder_cls3_val = "M:\\data22-brs\\AARISH\\01\\nobackup\\imagenet\\training_256x256_rgb\\val\\n01484850";
 
     
     // Get paths of images and labels as int from the folder paths
     std::pair<std::vector<std::string>, std::vector<int>> pair_images_labels = load_data_from_folder({dataset_folder_cls1,dataset_folder_cls2, dataset_folder_cls3});
-    
-    std::vector<std::string> list_images = pair_images_labels.first;
-    std::vector<int> list_labels = pair_images_labels.second;
-    
+    std::pair<std::vector<std::string>, std::vector<int>> pair_images_labels_val = load_data_from_folder({ dataset_folder_cls1_val,dataset_folder_cls2_val, dataset_folder_cls3_val });
+
+//    std::pair<std::vector<std::string>, std::vector<int>> pairs_training, pairs_validation;
+//    splitData(pair_images_labels, 0.8, pairs_training, pairs_validation);
+
+   
     // Initialize CustomDataset class and read data
-    auto custom_dataset = CustomDataset(list_images, list_labels).map(torch::data::transforms::Stack<>());
-   auto netx = vision::models::ResNet34(3);
+  //  auto custom_dataset_trn = CustomDataset(pairs_training.first, pairs_training.second).map(torch::data::transforms::Stack<>());
+  //  auto custom_dataset_valid = CustomDataset(pairs_validation.first, pairs_validation.second).map(torch::data::transforms::Stack<>());
+    auto custom_dataset_trn = CustomDataset(pair_images_labels.first, pair_images_labels.second).map(torch::data::transforms::Stack<>());
+    auto custom_dataset_valid = CustomDataset(pair_images_labels_val.first, pair_images_labels_val.second).map(torch::data::transforms::Stack<>());
+
+    auto netx = vision::models::ResNet34(3);
     auto net = EfficientNetV1(GlobalParams{ 1.0, 1.0, 224, 0.2 }, 3);
     try {
         net.get()->to(torch::kCUDA);
@@ -275,19 +326,16 @@ int main(int argc, const char * argv[]) {
         std::cout << e.what() << std::endl;
     }
    
-    // Resource: https://discuss.pytorch.org/t/how-to-load-the-prebuilt-resnet-models-or-any-other-prebuilt-models/40269/8
-    // For VGG: 512 * 14 * 14, 2
-
-    //torch::nn::Linear lin(512, 3); // the last layer of resnet, which we want to replace, has dimensions 512x1000
 
 //    torch::optim::Adam opt(net.get()->parameters(), torch::optim::AdamOptions(1e-3 /*learning rate*/));
 //    torch::optim::SGD opt(net.get()->parameters(), torch::optim::SGDOptions(1e-3));
 
     torch::optim::SGDAGC opt(net.get()->parameters(), torch::optim::SGDAGCOptions(1.0));
 
-    auto data_loader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>(std::move(custom_dataset), 4);
+    auto data_loader_trn = torch::data::make_data_loader<torch::data::samplers::RandomSampler>(std::move(custom_dataset_trn), 4);
+    auto data_loader_valid = torch::data::make_data_loader<torch::data::samplers::RandomSampler>(std::move(custom_dataset_valid), 4);
 
-    train(net, data_loader, opt, custom_dataset.size().value());
+    train(net, data_loader_trn, data_loader_valid, opt, custom_dataset_trn.size().value(), custom_dataset_valid.size().value());
 
     return 0;
 }
